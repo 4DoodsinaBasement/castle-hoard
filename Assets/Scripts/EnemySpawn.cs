64d6b6e67f7d1ff9591216632e6f270853d7624c
nate.online <nate@travelwt.com>
2019-04-24 22:05:26 -0600

    uploaded prototype

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject enemyObject;
+    public int numberEnemies = 10;
+    public float radius = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 spawnLocation;
+        
+        for (int i = 0; i < numberEnemies; i++)
+        {
+            spawnLocation = Random.insideUnitCircle.normalized * (radius * (1 + Random.value));
+            Instantiate(enemyObject, spawnLocation, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PointerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerController : MonoBehaviour
+{
+    public Vector3 direction;
+    float angle;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePointer();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+        }
+    }
+
+    void MovePointer()
+    {
+        direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
