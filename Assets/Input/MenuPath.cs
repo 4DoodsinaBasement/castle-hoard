660b0fc59f6505ee9cd27d35c8a546e0196d192b
nate.online <nate@travelwt.com>
2019-05-11 11:47:36 -0600

    started building controller menu

---
+++ Assets/Input/MenuPath.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MenuNavDirection {North, NE, East, SE, South, SW, West, NW}
+
+public class MenuPath
+{
+    public MenuNavDirection navDirection;
+
+
+    
+    
+}
