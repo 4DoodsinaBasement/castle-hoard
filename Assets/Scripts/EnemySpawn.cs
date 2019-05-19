87d59ee7179b164c9b9d4f9a82d98bb760eb969e
jt <j4c0bh3rrm4nn@gmail.com>
2019-05-18 19:41:26 -0600

    created initial spawn system up to a wave

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -4,25 +4,46 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    public GameObject enemyObject;
-    public int numberEnemies = 10;
-    public float radius = 10;
-    
+    public GameObject enemyToSpawn;
+    public GameObject SpawnType; 
+    private GameObject[] eligibleSpawnLocations;
+    public int enemiesToSpawn;
+    private int arrayEnd;
+    int offset = 0;
+
     // Start is called before the first frame update
-    void Start()
+    public void Spawn()
     {
-        Vector3 spawnLocation;
-        
-        for (int i = 0; i < numberEnemies; i++)
+        eligibleSpawnLocations =  GameObject.FindGameObjectsWithTag(SpawnType.tag);
+        arrayEnd = eligibleSpawnLocations.Length -1;
+
+        for (int i = 0; i<enemiesToSpawn; i++)
         {
-            spawnLocation = Random.insideUnitCircle.normalized * (radius * (1 + Random.value));
-            Instantiate(enemyObject, spawnLocation, Quaternion.identity);
+            if(arrayEnd == -1)
+            {
+                arrayEnd = eligibleSpawnLocations.Length-1;
+                offset++;
+            }
+            int j = Random.Range(0, arrayEnd);
+            GameObject locationToSpawn = eligibleSpawnLocations[j];
+            Vector3 location = new Vector3(
+                locationToSpawn.transform.position.x, // + (offset *5),
+                locationToSpawn.transform.position.y,
+                locationToSpawn.transform.position.z
+                );
+  
+            Instantiate(enemyToSpawn, location, locationToSpawn.transform.rotation);
+            swap(arrayEnd, j);
+            arrayEnd--;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void swap(int a, int b)
     {
-        
+        GameObject temp = eligibleSpawnLocations[a];
+        eligibleSpawnLocations[a] = eligibleSpawnLocations[b];
+        eligibleSpawnLocations[b] = temp;
     }
+
+
 }
---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+    
+    public int delay;
+    int nextGroup = 0;
+    float targetTime = 0;
+    public GameObject[] groups;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((Time.time >= targetTime + delay) && (nextGroup < groups.Length))
+        {
+            EnemySpawn[] squads = groups[nextGroup].GetComponents<EnemySpawn>();
+            for (int j = 0; j < squads.Length; j++)
+            {
+                groups[nextGroup].GetComponent<EnemySpawn>().Spawn();
+            }
+            nextGroup++;
+            targetTime = Time.time + delay;
+        }
+
+    }
+}
